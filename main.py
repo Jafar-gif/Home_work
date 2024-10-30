1500d1e959c26408dff91c3ebb48be2e55c2377d
Jafar-gif <jafarortukov97@gmail.com>
2024-10-30 18:00:25 +0300

    изменил файл main.pay

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from fake_muth import *
+from true_math import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+result1 = fake_divide(69,3)
+result2 = fake_divide(3,0)
+result3 = true_divide(49,7)
+result4 = true_divide(15,0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
